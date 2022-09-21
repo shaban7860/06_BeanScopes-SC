da1b4e8a9a16964d14e3e0c3f95228367689a22d
shaban7860 <113911526+shaban7860@users.noreply.github.com>
2022-09-21 17:32:49 +0530

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,55 @@
+package com.mindgate.pojo;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class Address {
+	private String address1;
+	private String address2;
+	private String address3;
+		
+	public Address() {
+		System.out.println("default constructor of Address class");
+	}
+	
+	public Address(String address1, String address2, String address3) {
+		super();
+		this.address1 = address1;
+		this.address2 = address2;
+		this.address3 = address3;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getAddress3() {
+		return address3;
+	}
+
+	public void setAddress3(String address3) {
+		this.address3 = address3;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [address1=" + address1 + ", address2=" + address2 + ", address3=" + address3 + "]";
+	}
+	
+	
+
+}
